db90f4f7c151b460ea2fe7d5ecb05e6bb80a907c
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-29 03:41:49 +0000

    Completed lightCoin-Crypto assignment w2 d5

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
-let balance = 500.00;
+//let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this._transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let result = 0;
+    for (const transaction of this._transactions) {
+      result += transaction.value;
+    }
+    return result;
   }
 
+  addTransaction(transaction) {
+    this._transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this._amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this._amount;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this._amount;
+  }
+  isAllowed() {
+    return this._amount <= this.account.balance;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(70.00, myAccount);
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
